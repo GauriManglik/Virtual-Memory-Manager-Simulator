e3cd22f78e01aa4fdd7d9f68a30ce7e02b1f01eb
GauriManglik <gaurimanglik01@gmail.com>
2025-04-10 13:14:44 +0530

    Fifo Page Replacement Algorithm

---
+++ fifo.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void fifo(){
+
+}
+
+int main(){
+    int numOfpages,frames;
+    cin>>numOfpages;
+    cin>>frames;
+    vector<int>pages;
+    for(int i=0;i<numOfpages;i++){
+        cin>>pages[i];
+    }
+}
