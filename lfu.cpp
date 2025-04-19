40108baff7528e4a3620fa10e0a6c60aa78a4440
GauriManglik <gaurimanglik01@gmail.com>
2025-04-19 21:11:23 +0530

    LFU Page Replacement Algorithm

--- lfu.cpp
+++ lfu.cpp
@@ -1,9 +1,55 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void lfu(int frames, int pg, vector<int> &pages, int &pageFaults){
-    
+void lfu(int frames, int pg, vector<int> &pages, int &pageFaults)
+{
+    unordered_set<int> s;
+    unordered_map<int, int> freq;
+
+    for (int i = 0; i < pages.size(); i++)
+    {
+        int currPage = pages[i];
+
+        if (s.size() < frames)
+        {
+            if (s.find(currPage) == s.end())
+            {
+                s.insert(currPage);
+                pageFaults++;
+                freq[currPage] = 1;
+            }
+            else
+            {
+                freq[currPage]++;
+            }
+        }
+        else
+        {
+            if (s.find(currPage) == s.end())
+            {
+                int minFreq = INT_MAX, val = -1;
+                for (auto it : s)
+                {
+                    if (freq[it] < minFreq)
+                    {
+                        minFreq = freq[it];
+                        val = it;
+                    }
+                }
+                s.erase(val);
+                freq.erase(val);
+                s.insert(currPage);
+                pageFaults++;
+                freq[currPage] = 1;
+            }
+            else
+            {
+                freq[currPage]++;
+            }
+        }
+    }
 }
+
 
 int main(){
     int numOfpages, frames, pageFaults = 0;
